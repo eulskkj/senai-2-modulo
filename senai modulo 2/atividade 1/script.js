ccbddf101bf24a6ac5d5a93cadfeed56ea8c63d6
eulskkj <pikapaybeludo@gmail.com>
2025-08-11 18:55:52 +0000

    salvar

--- senai modulo 2/atividade 1/script.js
+++ senai modulo 2/atividade 1/script.js
@@ -1,4 +1,6 @@
-
+document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById("gerarBotao").addEventListener("click", gerarBotao);
+});
 
 function gerarBotao() {
     const idadeInput = document.getElementById("idadeInput");
@@ -6,25 +8,21 @@ function gerarBotao() {
     const resultadoDiv = document.getElementById("resultado");
     resultadoDiv.innerHTML = ""; // Limpa o conteúdo anterior
 
-
-    if(idade < 16) {
-       resultadoDiv.innerHTML = "<p>Não pode votar</p>"; 
-    } else if(idade >= 16 && idade < 17) {
-        resultadoDiv.innerHTML = "<p>Pode votar, mas não é obrigatório</p>";
-    } else if(idade >= 18 && idade < 64) {
-        resultadoDiv.innerHTML = "<p>Pode votar e é obrigatório</p>";
-    } else if(idade >= 64) {
-        resultadoDiv.innerHTML = "<p>Pode votar, mas não é obrigatório</p>";
-    }   
-    else {
+    if (isNaN(idade) || idade < 0 || idade > 120) {
         resultadoDiv.innerHTML = "<p>Idade inválida</p>";
-    }         
-    
+        return;
+    }
 
+    if (idade < 16) {
+        resultadoDiv.innerHTML = "<p>Não pode votar</p>";
+    } else if (idade >= 16 && idade < 18) {
+        resultadoDiv.innerHTML = "<p>Pode votar, mas não é obrigatório</p>";
+    } else if (idade >= 18 && idade < 65) {
+        resultadoDiv.innerHTML = "<p>Pode votar e é obrigatório</p>";
+    } else {
+        resultadoDiv.innerHTML = "<p>Pode votar, mas não é obrigatório</p>";
+    }
 }
-
-
-
 
 
 
